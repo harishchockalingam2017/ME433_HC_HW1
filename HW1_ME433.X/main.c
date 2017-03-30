f9c826a369eadb000d195d4c75b1ccaf1493c625
harishchockalingam <harishchockalingam2017@u.northwestern.edu>
2017-03-29 22:46:43 -0500

    main.c

--- HW1_ME433.X/main.c
+++ HW1_ME433.X/main.c
@@ -52,13 +52,18 @@ int main() {
 
     // disable JTAG to get pins back
     DDPCONbits.JTAGEN = 0;
-
+    
     // do your TRIS and LAT commands here
-
+    TRISAbits.TRISA4=0;
+    LATAbits.LATA4=1;
+    
     __builtin_enable_interrupts();
-
+    
     while(1) {
+        _CP0_SET_COUNT(0);
+        while(_CP0_GET_COUNT()<1000){;}
 	    // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
 		  // remember the core timer runs at half the sysclk
     }
+    return 0;
 }
